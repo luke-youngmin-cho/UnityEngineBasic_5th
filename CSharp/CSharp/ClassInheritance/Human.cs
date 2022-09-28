aa3127937099c9deb184643ccad09b9eeec4a066
YoungMin Cho (Luke) <83196845+luke-youngmin-cho@users.noreply.github.com>
2022-09-28 19:50:25 +0900

    CSharp - HorseRacing, Class Inheritance
    1. HorseRacing
    - Complete
    
    2. Class Inheritance
    - About inheritance
    - usage of abstract keyword

---
+++ CSharp/CSharp/ClassInheritance/Human.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassInheritance
+{
+    // 상속 
+    // 클래스이름 뒤에 : 쓰고 상속받을 클래스 이름 씀
+    internal class Human : Creature
+    {
+        // override 키워드
+        // 부모 클래스의 멤버를 재정의 할 때 쓰는 키워드
+        public override void Breath()
+        {
+            Console.WriteLine("Human is breathing");
+        }
+    }
+}
---
+++ CSharp/CSharp/ClassInheritance/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ClassInheritance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 추상화 클래스는 인스턴스화 할 수 없다
+            //Creature creature = new Creature();
+
+            Human human = new Human();
+            human.Breath();
+        }
+    }
+}
