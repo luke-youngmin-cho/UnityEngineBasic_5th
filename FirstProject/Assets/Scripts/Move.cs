d81634dc0f8e1067f6b64715581017ab2075cc61
YoungMin Cho (Luke) <83196845+luke-youngmin-cho@users.noreply.github.com>
2022-10-18 19:51:29 +0900

    FirstProject- Simple character movement, Models
    1. Simple character movement
    - Get mouse input via Input class
    - Rotate & Quaternion
    
    2. Models
    - Added robot & some materials

--- FirstProject/Assets/Scripts/Move.cs
+++ FirstProject/Assets/Scripts/Move.cs
@@ -4,19 +4,32 @@ using UnityEngine;
 
 public class Move : MonoBehaviour
 {
+    public float MoveSpeed = 3.0f;
+    public float RotateSpeed = 360.0f;
     float _h;
     float _v;
+    float _r;
 
     private void Update()
     {
         _h = Input.GetAxisRaw("Horizontal");
         _v = Input.GetAxisRaw("Vertical");
+        _r = Input.GetAxis("Mouse X");
     }
 
     private void FixedUpdate()
     {
         Vector3 dir = new Vector3(_h, 0.0f, _v).normalized;
+        // 이동벡터 = 속도 * 시간
+        // 이동벡터변화량 = 속도 * 시간변화량
+        //transform.position += dir * Time.fixedDeltaTime;
+        transform.Translate(dir * MoveSpeed * Time.fixedDeltaTime, Space.World);
 
-        transform.position += dir * Time.fixedDeltaTime;
+        // Quaternion
+        // 사원수, 각도를 4개의 원소로 표현하기위한 체계
+        // Eular 각도를 사용하면 짐벌락 문제가 생김.
+        // 짐벌락 : 두개의 축이 겹치면서 자유도를 상실하는 현상
+        // 연산이 Ealar 각도체계보다 가볍다
+        transform.Rotate(Vector3.up * _r * RotateSpeed * Time.fixedDeltaTime);
     }
 }
--- FirstProject/Assets/Scripts/Test.cs
+++ FirstProject/Assets/Scripts/Test.cs
@@ -6,68 +6,113 @@ public class Test : MonoBehaviour
 {
     public int Num;
 
+    /// <summary>
+    /// Script Instance 가 처음 Load 될 때 한번 호출
+    /// (Script Instance : GameObject 에 붙인 Script)
+    /// Load 되는 시기는 해당 Script Instance 를 가지고있는 GameObject 가 처음 생성될때
+    /// Script Instance 의 활성/비활성 유무와 관계없이 호출
+    /// </summary>
     private void Awake()
     {
         Debug.Log("[Test] : Awake");
     }
 
+    /// <summary>
+    /// Script Instance 가 활성화 될 때 마다 호출
+    /// 해당 Script Instance 를 가지고있는  GameObject 가 활성/ 비활성화 될때도 영향을 받음
+    /// </summary>
     private void OnEnable()
     {
         Debug.Log("[Test] : OnEnable");
     }
 
+    /// <summary>
+    /// Script를 GameObject 에 붙일때 처음 호출됨.
+    /// Script Instance 의 멤버 변수들을 초기화 해줌.
+    /// Inspector 창에서 수동으로 호출할 수 있음.
+    /// </summary>
     private void Reset()
     {
         Debug.Log("[Test] : Reset");
     }
 
+    /// <summary>
+    /// Update 함수가 호출되기 전에 한번 실행되는 함수
+    /// </summary>
     private void Start()
     {
         Debug.Log("[Test] : Start");
     }
 
+    /// <summary>
+    /// 게임 로직 매 프레임 마다 처음 호출되는 함수
+    /// </summary>
     private void Update()
     {
         Debug.Log("[Test] : Update");
     }
 
+    /// <summary>
+    /// 게임 로직 매 프레임 마다 마지막에 호출되는 함수
+    /// </summary>
     private void LateUpdate()
     {
         Debug.Log("[Test] : LateUpdate");
     }
 
+    /// <summary>
+    /// 물리 연산을 위한 프레임 마다 호출되는 함수
+    /// </summary>
     private void FixedUpdate()
     {
         Debug.Log("[Test] : FixedUpdate");
     }
 
+    /// <summary>
+    /// Editor에서 Gizmo(에디터상의 그래픽적인 부가 요소들) 들을 그릴 때 호출되는 함수
+    /// </summary>
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.green;
         Gizmos.DrawCube(transform.position, Vector3.one);
     }
 
+    /// <summary>
+    /// UI 이벤트를 처리하기위한 함수
+    /// </summary>
     private void OnGUI()
     {
         Event e = Event.current;
         Debug.Log(e.mousePosition);
     }
 
+    /// <summary>
+    /// 어플리케이션이 일시정지될때 호출
+    /// </summary>
     private void OnApplicationPause(bool pause)
     {
         Debug.Log($"[Test] : paused = {pause}");
     }
 
+    /// <summary>
+    /// 어플리케이션이 종료될때 호출
+    /// </summary>
     private void OnApplicationQuit()
     {
         Debug.Log("[Test] : application quit ");
     }
 
+    /// <summary>
+    /// Script Instance 가 비활성화될 때 마다 호출
+    /// </summary>
     private void OnDisable()
     {
         Debug.Log("[Test] : Disabled");
     }
 
+    /// <summary>
+    /// 파괴될 때 호출
+    /// </summary>
     private void OnDestroy()
     {
         Debug.Log("[Test] : Destroyed");
