6b8bf42780d3e3633c8c42feaf2936d4de8366a9
YoungMin Cho (Luke) <83196845+luke-youngmin-cho@users.noreply.github.com>
2022-10-25 19:49:49 +0900

    RollADice - Concept , Map
    1. Concept
    - Created
    
    2. Map
    - Made with UI
    - Added some scripts for map tiles

---
+++ RollADice/Assets/02.Scripts/TileBonusGoldenDice.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileBonusGoldenDice : Tile
+{
+    public override void OnHere()
+    {
+        base.OnHere();
+        // todo -> Increase golden dice number
+    }
+}
---
+++ RollADice/Assets/02.Scripts/TileBonusNormalDice.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileBonusNormalDice : Tile
+{
+    public override void OnHere()
+    {
+        base.OnHere();
+        // todo -> Increase normal dice number
+    }
+}
