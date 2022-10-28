db57d3e7a32e0b0b264f944c91c0a4e44b806592
YoungMin Cho (Luke) <83196845+luke-youngmin-cho@users.noreply.github.com>
2022-10-28 19:35:15 +0900

    RollADice - DiceAnimation
    DiceAnimation
    - Implemented events when animation started & finished.

--- RollADice/Assets/02.Scripts/DiceAnimationUI.cs
+++ RollADice/Assets/02.Scripts/DiceAnimationUI.cs
@@ -7,23 +7,40 @@ using UnityEngine.UI;
 using Random = UnityEngine.Random;
 public class DiceAnimationUI : MonoBehaviour
 {
+    public static DiceAnimationUI Instance;
+
     [SerializeField] private Image _image;
+    [SerializeField] private Button _normalDiceButton;
+    [SerializeField] private Button _goldenDiceButton;
+
     [SerializeField] private float _animationDelay;
     [SerializeField] private float _animationTime;
     private float _animationDelayTimer;
     private List<Sprite> _sprites;
 
-    public delegate void AnimationFinishedHandler(int diceValue);
-    public AnimationFinishedHandler OnAnimationFinished2;
+    public event Action OnAnimationStarted;
+    public event Action<int> OnAnimationFinished;
 
-    public Action<int> OnAnimationFinished;
+    public void PlayDiceAnimation(int diceValue)
+    {
+        StartCoroutine(E_DiceAnimation(diceValue));
+    }
 
     private void Awake()
     {
+        Instance = this;
         LoadSprites();
-    }
-
-    
+        OnAnimationStarted += () =>
+        {
+            _normalDiceButton.interactable = false;
+            _goldenDiceButton.interactable = false;
+        };
+        OnAnimationFinished += (diceValue) =>
+        {
+            _normalDiceButton.interactable = true;
+            _goldenDiceButton.interactable = true;
+        };
+    }    
 
     private void LoadSprites()
     {
@@ -34,8 +51,13 @@ public class DiceAnimationUI : MonoBehaviour
     // 어떤 함수가 반환 될 때 다른 함수를 호출해주고, 그 다른 함수도 반환할때 어떤 함수를 호출해주는 
     // 상호 협동하는 관계의 함수관계를 협동 루틴 이라고 한다.
 
+    // Monobehavior 의 코루틴은 해당 코루틴을 호출한 MonoBehavior 의 Update 이벤트와 Coroutine을 형성한다.
+    // 즉, 코루틴을 실행한 주체 MonoBehavior 가 비활성화 될 경우 (Update 이벤트함수가 호출되지 못하게 될 경우)
+    // , 해당 Coroutine 은 돌아가지 않는다.
     IEnumerator E_DiceAnimation(int diceValue)
     {
+        OnAnimationStarted?.Invoke();
+
         float elapsedTime = 0.0f;
         while (elapsedTime < _animationTime)
         {
@@ -51,6 +73,8 @@ public class DiceAnimationUI : MonoBehaviour
         }
 
         _image.sprite = _sprites[diceValue - 1];
-        OnAnimationFinished(diceValue);
+        
+        // ? : Null조건 연산자
+        OnAnimationFinished?.Invoke(diceValue);
     }
 }
--- RollADice/Assets/02.Scripts/GameManager.cs
+++ RollADice/Assets/02.Scripts/GameManager.cs
@@ -87,24 +87,28 @@ public class GameManager : MonoBehaviour
     private List<TileStar> _tileStars = new List<TileStar>();
     private int _tilesCount;
     private int _current = -1;
-    
+    private bool _isPlayerMoving = false;
 
     public void RollANormalDice()
     {
-        if (NormalDiceNum > 0)
+        if (NormalDiceNum > 0 &&
+            _isPlayerMoving == false)
         {
+            _isPlayerMoving = true;
             NormalDiceNum--;
             int diceValue = Random.Range(1, 7);
-            MovePlayer(diceValue);
+            DiceAnimationUI.Instance.PlayDiceAnimation(diceValue);
         }
     }
 
     public void RollAGoldenDice(int diceValue)
     {
-        if (GoldenDiceNum > 0)
+        if (GoldenDiceNum > 0 &&
+            _isPlayerMoving == false)
         {
+            _isPlayerMoving = true;
             GoldenDiceNum--;
-            MovePlayer(diceValue);
+            DiceAnimationUI.Instance.PlayDiceAnimation(diceValue);
         }
     }
 
@@ -166,5 +170,11 @@ public class GameManager : MonoBehaviour
         _tilesCount = _tiles.Count;
         _current = -1;
         Direction = Constants.DIRECTION_POSITIVE;
+    }
+
+    private void Start()
+    {
+        DiceAnimationUI.Instance.OnAnimationFinished += MovePlayer;
+        DiceAnimationUI.Instance.OnAnimationFinished += (diceValue) => _isPlayerMoving = false;
     }
 }
