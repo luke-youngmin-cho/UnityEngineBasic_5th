f462a54f506c495252c951727e071241b6c17c94
YoungMin Cho (Luke) <83196845+luke-youngmin-cho@users.noreply.github.com>
2022-11-16 20:03:32 +0900

    Platformer2D - EnemyController
    Created NepenthesController derived from EnemyController

--- Platformer2D/Assets/02.Scripts/EnemyController.cs
+++ Platformer2D/Assets/02.Scripts/EnemyController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class EnemyController : MonoBehaviour
@@ -39,7 +40,7 @@ public class EnemyController : MonoBehaviour
     [SerializeField] private float _aiBehaviorTimeMin = 0.1f;
     [SerializeField] private float _aiBehaviorTimeMax = 2.0f;
     private float _aiBehaviorTimer;
-    [SerializeField] private LayerMask _targetLayer;
+    [SerializeField] protected LayerMask TargetLayer;
 
     // Movement
     private const int DIRECTION_LEFT = -1;
@@ -68,8 +69,9 @@ public class EnemyController : MonoBehaviour
     [SerializeField] private bool _moveEnable = true;
     [SerializeField] private bool _movable = false;
     [SerializeField] private float _moveSpeed = 1.0f;
-    private Rigidbody2D _rb;
+    protected Rigidbody2D Rb;
     private CapsuleCollider2D _col;
+    private Collider2D _target;
 
     // animation
     private Animator _animator;
@@ -78,7 +80,7 @@ public class EnemyController : MonoBehaviour
 
     private void Awake()
     {
-        _rb = GetComponent<Rigidbody2D>();
+        Rb = GetComponent<Rigidbody2D>();
         _col = GetComponent<CapsuleCollider2D>();
         _animator = GetComponent<Animator>();        
     }
@@ -92,7 +94,7 @@ public class EnemyController : MonoBehaviour
     private void FixedUpdate()
     {
         if (_moveEnable && _movable)
-            _rb.MovePosition(_rb.position + Direction * Vector2.right * _moveSpeed * Time.fixedDeltaTime);
+            Rb.MovePosition(Rb.position + Direction * Vector2.right * _moveSpeed * Time.fixedDeltaTime);
     }
 
     private void UpdateAI()
@@ -100,18 +102,17 @@ public class EnemyController : MonoBehaviour
         if (Current == States.Hurt || Current == States.Die)
             return;
 
-        Collider2D target = null;
         switch (_ai)
         {
             case AI.Idle:
                 break;
             case AI.Think:
                 {
-                    target = Physics2D.OverlapCircle(_rb.position, _aiDetectRange, _targetLayer);
-                    if (target != null)
+                    _target = Physics2D.OverlapCircle(Rb.position, _aiDetectRange, TargetLayer);
+                    if (_target != null)
                     {
                         if (_aiAttackEnable &&
-                            Vector2.Distance(_rb.position, target.transform.position) <= _aiAttackRange)
+                            Vector2.Distance(Rb.position, _target.transform.position) <= _aiAttackRange)
                         {
                             _ai = AI.AttackTarget;
                             ChangeState(States.Attack);
@@ -173,13 +174,30 @@ public class EnemyController : MonoBehaviour
                 break;
             case AI.FollowTarget:
                 {
-                    if (_rb.position.x < target.transform.position.x + _col.size.x)
+                    // 타겟이 범위를 벗어남
+                    if (_target == null ||
+                        Vector2.Distance(Rb.position, _target.transform.position) > _aiDetectRange)
                     {
-                        Direction = DIRECTION_RIGHT;
+                        _ai = AI.Think;
                     }
-                    else if (_rb.position.x > target.transform.position.x - _col.size.x)
+                    // 타겟이 공격 범위 내에 들어옴
+                    else if (_aiAttackEnable &&
+                             Vector2.Distance(Rb.position, _target.transform.position) < _aiAttackRange)
                     {
-                        Direction = DIRECTION_LEFT;
+                        ChangeState(States.Attack);
+                        _ai = AI.AttackTarget;
+                    }
+                    // 둘다 아니면 그냥 따라감
+                    else
+                    {
+                        if (Rb.position.x < _target.transform.position.x - _col.size.x)
+                        {
+                            Direction = DIRECTION_RIGHT;
+                        }
+                        else if (Rb.position.x > _target.transform.position.x + _col.size.x)
+                        {
+                            Direction = DIRECTION_LEFT;
+                        }
                     }
                 }
                 break;
@@ -350,13 +368,28 @@ public class EnemyController : MonoBehaviour
             case 0:
                 // nothing to do
                 break;
+            // 공격 애니메이션 재생
+            //-----------------------------------------------------------------
             case 1:
                 {
                     _animator.Play("Attack");
                     _attackStep++;
                 }
                 break;
+            // 공격 시전 시간
+            //-----------------------------------------------------------------
             case 2:
+                {
+                    if (_animator.GetCurrentAnimatorStateInfo(0).normalizedTime >= 0.7f)
+                    {
+                        AttackBehavior();
+                        _attackStep++;
+                    }
+                }
+                break;
+            // 공격 모션 종료 대기
+            //------------------------------------------------------------------
+            case 3:
                 {
                     if (_animator.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1.0f)
                     {
@@ -373,6 +406,7 @@ public class EnemyController : MonoBehaviour
 
         return next;
     }
+    
 
     // Hurt 실행 가능 조건
     private bool CanHurt()
@@ -453,5 +487,21 @@ public class EnemyController : MonoBehaviour
         }
 
         return next;
+    }
+
+
+
+    protected virtual void AttackBehavior()
+    {
+
+    }
+
+    protected virtual void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, _aiDetectRange);
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, _aiAttackRange);
     }
 }
---
+++ Platformer2D/Assets/02.Scripts/NepenthesController.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NepenthesController : EnemyController
+{
+    [SerializeField] private Vector2 _attackBoxCastCenter;
+    [SerializeField] private Vector2 _attackBoxCastSize;
+    protected override void AttackBehavior()
+    {
+        base.AttackBehavior();
+        RaycastHit2D hit = Physics2D.BoxCast(origin: Rb.position + new Vector2(_attackBoxCastCenter.x * Direction, _attackBoxCastCenter.y),
+                                             size: _attackBoxCastSize,
+                                             angle: 0.0f,
+                                             direction: Vector2.zero,
+                                             distance: 0.0f,
+                                             layerMask: TargetLayer);
+
+        if (hit.collider != null)
+        {
+            Debug.Log("Casted target!");
+        }
+    }
+
+    protected override void OnDrawGizmos()
+    {
+        base.OnDrawGizmos();
+
+        if (Current == States.Attack)
+        {
+            Gizmos.color = Color.magenta;
+            Gizmos.DrawWireCube(transform.position + new Vector3(_attackBoxCastCenter.x * Direction, _attackBoxCastCenter.y),
+                                _attackBoxCastSize);
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.magenta;
+        Gizmos.DrawWireCube(transform.position + new Vector3(_attackBoxCastCenter.x * Direction, _attackBoxCastCenter.y),
+                            _attackBoxCastSize);
+    }
+}
